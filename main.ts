08de2480ff3f058348291f7c9f9daed565a0e06c
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-04-12 19:29:46 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,28 @@ input.onButtonPressed(Button.A, function () {
         RightArm = 0
     }
 })
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "L") {
+        EyesLeft.showImage(0)
+        pins.servoWritePin(AnalogPin.P2, LeftArm * 90)
+        basic.pause(500)
+        Eyes.showImage(0)
+        LeftArm += 1
+        if (LeftArm > 2) {
+            LeftArm = 0
+        }
+    }
+    if (receivedString == "R") {
+        EyesRight.showImage(0)
+        pins.servoWritePin(AnalogPin.P1, RightArm * 90)
+        basic.pause(500)
+        Eyes.showImage(0)
+        RightArm += 1
+        if (RightArm > 2) {
+            RightArm = 0
+        }
+    }
+})
 input.onButtonPressed(Button.B, function () {
     EyesLeft.showImage(0)
     pins.servoWritePin(AnalogPin.P2, LeftArm * 90)
@@ -23,6 +45,7 @@ let RightArm = 0
 let EyesLeft: Image = null
 let EyesRight: Image = null
 let Eyes: Image = null
+radio.setGroup(1)
 Eyes = images.createImage(`
     . . . . .
     . # . # .
