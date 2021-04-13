02cb31fa3e5993f3925cb9e8d58d4cda0fd560a0
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-04-13 13:30:44 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,42 @@
 input.onButtonPressed(Button.A, function () {
     EyesRight.showImage(0)
-    pins.servoWritePin(AnalogPin.P1, RightArm * 90)
+    pins.servoWritePin(AnalogPin.P1, RightArm * 45)
     basic.pause(500)
     Eyes.showImage(0)
     RightArm += 1
-    if (RightArm > 2) {
+    if (RightArm > 6) {
         RightArm = 0
     }
 })
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "L") {
         EyesLeft.showImage(0)
-        pins.servoWritePin(AnalogPin.P2, LeftArm * 90)
+        pins.servoWritePin(AnalogPin.P2, LeftArm * 45)
         basic.pause(500)
         Eyes.showImage(0)
         LeftArm += 1
-        if (LeftArm > 2) {
+        if (LeftArm > 6) {
             LeftArm = 0
         }
     }
     if (receivedString == "R") {
         EyesRight.showImage(0)
-        pins.servoWritePin(AnalogPin.P1, RightArm * 90)
+        pins.servoWritePin(AnalogPin.P1, RightArm * 45)
         basic.pause(500)
         Eyes.showImage(0)
         RightArm += 1
-        if (RightArm > 2) {
+        if (RightArm > 6) {
             RightArm = 0
         }
     }
 })
 input.onButtonPressed(Button.B, function () {
     EyesLeft.showImage(0)
-    pins.servoWritePin(AnalogPin.P2, LeftArm * 90)
+    pins.servoWritePin(AnalogPin.P2, LeftArm * 45)
     basic.pause(500)
     Eyes.showImage(0)
     LeftArm += 1
-    if (LeftArm > 2) {
+    if (LeftArm > 6) {
         LeftArm = 0
     }
 })
